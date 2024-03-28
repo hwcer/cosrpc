6afb05327e96556b4204ad6898a95cc966262704
万超 黄 <hwclegend@163.com>
2024-03-28 18:13:53 +0800

    update

--- module.go
+++ module.go
@@ -5,30 +5,26 @@ import (
 	"github.com/hwcer/cosgo"
 )
 
-var mod *module
+var mod *Module
 
-func New() *module {
+func New() *Module {
 	if mod == nil {
-		mod = &module{
+		mod = &Module{
 			Module: cosgo.Module{Id: "rpcx"},
 		}
 	}
 	return mod
 }
 
-type module struct {
+type Module struct {
 	cosgo.Module
 }
 
-func (this *module) Init() (err error) {
+func (this *Module) Init() (err error) {
 	return this.Reload()
 }
 
-func (this *module) Start() (err error) {
-	//RPCX SERVER
-	if err = Server.Start(); err != nil {
-		return
-	}
+func (this *Module) Start() (err error) {
 	//RPCX CLIENT
 	if err = Client.Start(); err != nil {
 		return
@@ -36,13 +32,11 @@ func (this *module) Start() (err error) {
 	return
 }
 
-func (this *module) Close() (err error) {
-	_ = Client.Close()
-	_ = Server.Close()
-	return
+func (this *Module) Close() (err error) {
+	return Client.Close()
 }
 
-func (this *module) Reload() (err error) {
+func (this *Module) Reload() (err error) {
 	if err = cosgo.Config.Unmarshal(Options); err != nil {
 		return
 	}
